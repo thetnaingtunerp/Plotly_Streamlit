8bf676f2145c29e4012deafc82399ad936760a0c
ThetNaingTun <67203393+thetnaingtunerp@users.noreply.github.com>
2026-01-08 21:04:20 +0630

    abc

---
+++ abc/eda.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
