7bbb1e9ccb7986732fb7b903be43052746cd3984
ThetNaingTun <67203393+thetnaingtunerp@users.noreply.github.com>
2026-01-09 12:24:37 +0630

    cph and no inform

--- abc/eda.py
+++ abc/eda.py
@@ -1,9 +1,92 @@
 import pandas as pd
 import streamlit as st
+import os
+import warnings
 
-uploaded_files = st.file_uploader(
-    "Upload data", accept_multiple_files=True, type="csv"
-)
-for uploaded_file in uploaded_files:
-    df = pd.read_csv(uploaded_file)
-    st.write(df)
+warnings.filterwarnings("ignore")
+
+page_config = {
+    "page_title": "ABC",
+    "page_icon": "📊",
+    "layout": "wide",
+}
+
+st.set_page_config(**page_config)
+
+
+        
+
+
+# @st.cache_data
+def get_dataset(name='abc.xlsx'):
+    data = pd.read_excel(name, sheet_name='Main DFR', engine='openpyxl')
+    # localdataset = pd.read_excel(abc.xlsx, sheet_name='Main DFR', engine='openpyxl')
+    dataset = data.dropna(how='all')
+    dataset['Filling Date'] = pd.to_datetime(dataset['Filling Date'], format='%d-%m-%Y')
+    dataset['Year'] = dataset['Filling Date'].dt.year
+    dataset['status'] = dataset['status'].apply(lambda x: x if x >= 0.5 else 0)
+    dataset['inform'] = dataset['Water Level'].apply(lambda x: 1 if x == 'No Inform' else 0)
+    return dataset
+
+
+# Create form
+# with st.form(key="data_form"):
+#     name = st.text_input("Copy and Pate Data URL")
+#     submit_btn = st.form_submit_button("Submit")
+
+# if submit_btn:
+#     if not name:
+#         st.error("not fields are filled!")
+#     else:
+#         get_dataset(name)
+
+
+#Sidebar
+
+year_selection = st.sidebar.header('Filters')
+sidebar = st.sidebar.header('Dataset Overview')
+
+with sidebar:
+    anchor_selection = sidebar.selectbox('Anchor ID', get_dataset()['Anchor ID'].unique())
+
+# Year Selection to Sidebar
+with year_selection:
+    # year_selection = st.sidebar.slider(get_dataset()['Year'].min(), get_dataset()['Year'].max(), get_dataset()['Year'].min(), key='year_slider')
+    yselection = st.sidebar.slider('Year', int(get_dataset()['Year'].min()), int(get_dataset()['Year'].max()), (int(get_dataset()['Year'].min()), int(get_dataset()['Year'].max())))
+
+
+# st.title('Fueling Data Report')
+filter_fm = st.container()
+
+no_inform = st.container()
+home = st.container()
+
+
+with home:
+    st.subheader('Fueling Data Report')
+    data = get_dataset()
+    col1,col2 = st.columns(2)
+    with col1:
+        #filter data by anchor id
+        filtered_data = data[(data['Anchor ID'] == anchor_selection) & (data['Year'].isin(yselection))]
+        st.dataframe(filtered_data[['Team Leader','Filling Date','Filling Liters']])
+    with col2:
+        # bar chart 
+        st.bar_chart(data=filtered_data, x='Filling Date', y=['status','inform'], color=['#0000FF', '#FF0000'], )
+
+
+with no_inform:
+    col1,col2, col3 = st.columns(3)
+    with col1:
+        st.subheader('No Inform Cases by Year')
+        no_inform_data = data[(data['inform'] == 1) &  (data['Year'].isin(yselection))]
+        st.dataframe(no_inform_data[['Team Leader','Filling Date','Anchor ID','Filling Liters']])
+
+    with col2:
+        st.subheader('CPH High list')
+        cph_high = data[(data['status'] > 0) &  (data['Year'].isin(yselection))]
+        st.dataframe(cph_high[['Team Leader','Filling Date','Anchor ID']])
+    with col3:
+        st.subheader('No Inform CPH High list')
+        no_inform_cph_high = data[(data['inform'] == 1) & (data['status'] > 0) &  (data['Year'].isin(yselection))]
+        st.dataframe(no_inform_cph_high[['Team Leader','Filling Date','Anchor ID']])
